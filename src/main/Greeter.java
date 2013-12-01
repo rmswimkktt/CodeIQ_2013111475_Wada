7c4b8558c9e112a7f6a8eb82d4687f286cc4e5a3
D.Maruta <rmswimkktt@gmail.com>
2013-12-01 13:18:32 +0900

    問1昼「こんにちは」の実装

--- src/main/Greeter.java
+++ src/main/Greeter.java
@@ -1,10 +1,23 @@
 package main;
 
+import java.util.Calendar;
+
 
 public class Greeter {
 
 	public String greet() {
-		return "おはようございます";
+		Calendar calendar = getTime();
+		int hour = calendar.get(Calendar.HOUR_OF_DAY);
+		if(hour >= 5 && hour < 12){
+			return "おはようございます";
+		}
+		else if(hour >= 12 && hour < 18){
+			return "こんにちは";
+		}
+		return null;
 	}
-
+	
+	protected Calendar getTime(){
+		return Calendar.getInstance();
+	}
 }
--- src/test/main/GreeterTest.java
+++ src/test/main/GreeterTest.java
@@ -3,18 +3,52 @@ package test.main;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import main.Greeter;
 
 import org.junit.Test;
 
-
-public class GreeterTest {
-
+public class GreeterTest extends Greeter{
+	private Calendar calendar;
+	@Override
+	protected Calendar getTime() {
+		return calendar;
+	}
 	@Test
-	public void morning() {
-		Greeter greeter = new Greeter();
+	public void morning1() {
+		calendar = makeCalendar(5, 0, 0);
 		String expected = "おはようございます";
-		assertThat(greeter.greet(), is(expected));
+		assertThat(greet(), is(expected));
+	}
+	@Test
+	public void morning2() {
+		calendar = makeCalendar(11, 59, 59);
+		String expected = "おはようございます";
+		assertThat(greet(), is(expected));
 	}
 
+	@Test
+	public void middy1() {
+		calendar = makeCalendar(12, 0, 0);
+		String expected = "こんにちは";
+		assertThat(greet(), is(expected));
+	}
+
+	@Test
+	public void middy2() {
+		calendar = makeCalendar(17, 59, 59);
+		String expected = "こんにちは";
+		assertThat(greet(), is(expected));
+	}
+
+	private Calendar makeCalendar(int hour, int minute, int second){
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.HOUR_OF_DAY, hour);
+		calendar.set(Calendar.MINUTE, minute);
+		calendar.set(Calendar.SECOND, second);
+		calendar.set(Calendar.MILLISECOND, 0);
+		return calendar;
+	}
 }
